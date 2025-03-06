8a3b8fcffba26c4f286a42d5bac1668b97782a8e
yahmedd043 <yahmedd043@gmail.com>
2025-03-06 19:05:13 +0000

    location.java implemented

--- battleship/app/src/main/java/bcc/battleship/Location.java
+++ battleship/app/src/main/java/bcc/battleship/Location.java
@@ -2,55 +2,69 @@ package bcc.battleship;
 
 public class Location {
     // Instance variables
-    boolean containsShip;
+    boolean hasShip;
+    int status;
 
     // Constructor. Initializes with no ship and status UNGUESSED.
     public Location() {
-
+        hasShip = false;
+        status = Constants.UNGUESSED;
     }
 
     // Was this Location a hit?
     public boolean checkHit() {
+        if (status == Constants.HIT)
+        {
+            return true;
+        }
         return false;
     }
 
     // Was this location a miss?
     public boolean checkMiss() {
+        if (status == Constants.MISSED)
+        {
+            return true;
+        }
         return false;
     }
 
     // Was this location unguessed?
     public boolean isUnguessed() {
+        if (status == Constants.UNGUESSED)
+        {
+            return true;
+        } 
         return false;
     }
 
     // Mark this location as a hit.
     public void markHit() {
-
+        status = Constants.HIT;
     }
 
     // Mark this location as a miss.
     public void markMiss() {
-
+        status = Constants.MISSED;
     }
 
     // Return whether or not this location has a ship.
     public boolean hasShip() {
-        return false;
+        return hasShip;
     }
 
     // Set whether this location has a ship.
     public void setShip(boolean val) {
-
+        hasShip = val;
     }
 
     // Set the status of this Location.
     public void setStatus(int status) {
-
+        this.status = status;
     }
 
     // Get the status of this Location.
     public int getStatus() {
-        return 0;
+        return status;
     }
 }
